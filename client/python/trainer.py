012d72886ba120716db73400e82824b6953b1087
acgzone <acgzone1264@gmail.com>
2023-11-05 12:44:30 +0800

    new file:   client/python/trainer.py

---
+++ client/python/trainer.py
@@ -0,0 +1,349 @@
+import json
+import socket
+from base import *
+from req import *
+from resp import *
+from config import config
+from ui import UI
+import subprocess
+import logging
+from threading import Thread
+from itertools import cycle, count
+from time import sleep
+from logger import logger
+import math
+import random
+from collections import namedtuple, deque
+
+import sys
+import termios
+import tty
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+# record the context of global data
+gContext = {
+    "playerID": -1,
+    "gameOverFlag": False,
+    "prompt": (
+        "Take actions!\n"
+        "'w': move up\n"
+        "'s': move down\n"
+        "'a': move left\n"
+        "'d': move right\n"
+        "'blank': place bomb\n"
+    ),
+    "steps": ["⢿", "⣻", "⣽", "⣾", "⣷", "⣯", "⣟", "⡿"],
+    "gameBeginFlag": False,
+}
+
+
+class Client(object):
+    """Client obj that send/recv packet.
+    """
+    def __init__(self) -> None:
+        self.config = config
+        self.host = self.config.get("host")
+        self.port = self.config.get("port")
+        assert self.host and self.port, "host and port must be provided"
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._connected = False
+
+    def connect(self):
+        if self.socket.connect_ex((self.host, self.port)) == 0:
+            logger.info(f"connect to {self.host}:{self.port}")
+            self._connected = True
+        else:
+            logger.error(f"can not connect to {self.host}:{self.port}")
+            exit(-1)
+        return
+
+    def send(self, req: PacketReq):
+        msg = json.dumps(req, cls=JsonEncoder).encode("utf-8")
+        length = len(msg)
+        self.socket.sendall(length.to_bytes(8, sys.byteorder) + msg)
+        # uncomment this will show req packet
+        # logger.info(f"send PacketReq, content: {msg}")
+        return
+
+    def recv(self):
+        length = int.from_bytes(self.socket.recv(8), sys.byteorder)
+        result = b""
+        while resp := self.socket.recv(length):
+            result += resp
+            length -= len(resp)
+            if length <= 0:
+                break
+
+        # uncomment this will show resp packet
+        # logger.info(f"recv PacketResp, content: {result}")
+        packet = PacketResp().from_json(result)
+        return packet
+
+    def __enter__(self):
+        return self
+    
+    def close(self):
+        logger.info("closing socket")
+        self.socket.close()
+        logger.info("socket closed successfully")
+        self._connected = False
+    
+    @property
+    def connected(self):
+        return self._connected
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+        if traceback:
+            print(traceback)
+            return False
+        return True
+
+
+def cliGetInitReq():
+    """Get init request from user input."""
+    return InitReq(config.get("DQN"))
+
+
+def recvAndRefresh(ui: UI, client: Client):
+    """Recv packet and refresh ui."""
+    global gContext
+    resp = client.recv()
+
+    if resp.type == PacketType.ActionResp:
+        gContext["gameBeginFlag"] = True
+        gContext["playerID"] = resp.data.player_id
+        ui.player_id = gContext["playerID"]
+
+
+    while resp.type != PacketType.GameOver:
+        subprocess.run(["clear"])
+        ui.refresh(resp.data)
+        ui.display()
+        resp = client.recv()
+
+    print(f"Game Over!")
+
+    print(f"Final scores \33[1m{resp.data.scores}\33[0m")
+
+    if gContext["playerID"] in resp.data.winner_ids:
+        print("\33[1mCongratulations! You win! \33[0m")
+    else:
+        print(
+            "\33[1mThe goddess of victory is not on your side this time, but there is still a chance next time!\33[0m"
+        )
+
+    gContext["gameOverFlag"] = True
+    print("Game Finished")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+class DQN(nn.Module):
+
+    def __init__(self, n_observations, n_actions):
+        super(DQN, self).__init__()
+        self.layer1 = nn.Linear(n_observations, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
+
+# BATCH_SIZE is the number of transitions sampled from the replay buffer
+# GAMMA is the discount factor as mentioned in the previous section
+# EPS_START is the starting value of epsilon
+# EPS_END is the final value of epsilon
+# EPS_DECAY controls the rate of exponential decay of epsilon, higher means a slower decay
+# TAU is the update rate of the target network
+# LR is the learning rate of the ``AdamW`` optimizer
+BATCH_SIZE = 128
+GAMMA = 0.99
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 1000
+TAU = 0.005
+LR = 1e-4
+
+# Get number of actions 
+n_actions = len(ActionType)
+# Get the number of state observations
+state, info = env.reset()
+n_observations = len(state)
+
+policy_net = DQN(n_observations, n_actions).to(device)
+target_net = DQN(n_observations, n_actions).to(device)
+target_net.load_state_dict(policy_net.state_dict())
+
+optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
+memory = ReplayMemory(10000)
+
+steps_done = 0
+
+def select_action(state):
+    global steps_done
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+        math.exp(-1. * steps_done / EPS_DECAY)
+    steps_done += 1
+    if sample > eps_threshold:
+        with torch.no_grad():
+            # t.max(1) will return the largest column value of each row.
+            # second column on max result is index of where max element was
+            # found, so we pick action with the larger expected reward.
+            return policy_net(state).max(1)[1].view(1, 1)
+    else:
+        return torch.tensor([[random.choice(ActionType)]], device=device, dtype=torch.long)
+
+episode_durations = []
+
+def optimize_model():
+    if len(memory) < BATCH_SIZE:
+        return
+    transitions = memory.sample(BATCH_SIZE)
+    # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
+    # detailed explanation). This converts batch-array of Transitions
+    # to Transition of batch-arrays.
+    batch = Transition(*zip(*transitions))
+
+    # Compute a mask of non-final states and concatenate the batch elements
+    # (a final state would've been the one after which simulation ended)
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                          batch.next_state)), device=device, dtype=torch.bool)
+    non_final_next_states = torch.cat([s for s in batch.next_state
+                                                if s is not None])
+    state_batch = torch.cat(batch.state)
+    action_batch = torch.cat(batch.action)
+    reward_batch = torch.cat(batch.reward)
+
+    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+    # columns of actions taken. These are the actions which would've been taken
+    # for each batch state according to policy_net
+    state_action_values = policy_net(state_batch).gather(1, action_batch)
+
+    # Compute V(s_{t+1}) for all next states.
+    # Expected values of actions for non_final_next_states are computed based
+    # on the "older" target_net; selecting their best reward with max(1)[0].
+    # This is merged based on the mask, such that we'll have either the expected
+    # state value or 0 in case the state was final.
+    next_state_values = torch.zeros(BATCH_SIZE, device=device)
+    with torch.no_grad():
+        next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0]
+    # Compute the expected Q values
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    # Compute Huber loss
+    criterion = nn.SmoothL1Loss()
+    loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+    # Optimize the model
+    optimizer.zero_grad()
+    loss.backward()
+    # In-place gradient clipping
+    torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
+    optimizer.step()
+
+if torch.cuda.is_available():
+    num_episodes = 600
+else:
+    num_episodes = 50
+
+for i_episode in range(num_episodes):
+    # Initialize the environment and get it's state
+    state, info = env.reset()
+    state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+    for t in count():
+        action = select_action(state)
+        observation, reward, terminated, truncated, _ = env.step(action.item())
+        reward = torch.tensor([reward], device=device)
+        done = terminated or truncated
+
+        if terminated:
+            next_state = None
+        else:
+            next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
+
+        # Store the transition in memory
+        memory.push(state, action, next_state, reward)
+
+        # Move to the next state
+        state = next_state
+
+        # Perform one step of the optimization (on the policy network)
+        optimize_model()
+
+        # Soft update of the target network's weights
+        # θ′ ← τ θ + (1 −τ )θ′
+        target_net_state_dict = target_net.state_dict()
+        policy_net_state_dict = policy_net.state_dict()
+        for key in policy_net_state_dict:
+            target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
+        target_net.load_state_dict(target_net_state_dict)
+
+        if done:
+            episode_durations.append(t + 1)
+            break
+
+def termPlayAPI():
+    ui = UI()
+    
+    with Client() as client:
+        client.connect()
+        
+        initPacket = PacketReq(PacketType.InitReq, cliGetInitReq())
+        client.send(initPacket)
+        
+        # IO thread to display UI
+        t = Thread(target=recvAndRefresh, args=(ui, client))
+        t.start()
+        
+        print(gContext["prompt"])
+        for c in cycle(gContext["steps"]):
+            if gContext["gameBeginFlag"]:
+                break
+            print(
+                f"\r\033[0;32m{c}\033[0m \33[1mWaiting for the other player to connect...\033[0m",
+                flush=True,
+                end="",
+            )
+            sleep(0.1)
+
+        while not gContext["gameOverFlag"]:
+            
+            # get from model and send request here
+            action = ActionReq(gContext["playerID"], ActionType.SILENT)
+            
+            if gContext["gameOverFlag"]:
+                break
+            
+            actionPacket = PacketReq(PacketType.ActionReq, action)
+            client.send(actionPacket)
+
+
+if __name__ == "__main__":
+    termPlayAPI()
