ccd973e79dfd7ab18ecee6ecdd26b54dc63a1c15
zya <1733179626@qq.com>
2023-11-16 23:50:00 +0800

    完成ParseMap 未测试

--- client/python/play.py
+++ client/python/play.py
@@ -31,6 +31,11 @@ import json
 import socket
 import sys
 
+#寻路库
+from pathfinding.core.diagonal_movement import DiagonalMovement
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
+#
 
 class Client(object):
     """Client obj that send/recv packet.
@@ -99,12 +104,50 @@ def cliGetInitReq():
     """Get init request from user input."""
     return InitReq(config.get("player_name"))
 
+#给定二维数组 返回 路径*215*len（Map）
+def find_all_paths(map_grid: List[List[Map]]) -> List[List[List[tuple[int, int]]]]:
+    paths = []
 
-def ParseMap(map:List[Map]) -> (List[List[Map]], List[List[List[tuple]]]):
+    
+
+    for start_x in range(len(map_grid)):
+        row_paths = []
+        for start_y in range(len(map_grid[0])):
+            start = map_grid[start_x][start_y]
+            
+            # 创建网格
+            matrix = [[1 for pnt in range(len(map_grid[0]))] for pnt in range(len(map_grid))]
+            for x in range(len(map_grid)):
+                for y in range(len(map_grid[0])):
+                    if any(obj.type == 3 for obj in map_grid[x][y].objs):
+                        matrix[x][y] = 0
+                    else:
+                        matrix[x][y] = 1
+
+            grid = Grid(matrix=matrix)
+
+            # 寻找路径
+            path_data = []
+            for end_x in range(len(map_grid)):
+                for end_y in range(len(map_grid[0])):
+                    end = map_grid[end_x][end_y]
+                    finder = AStarFinder(diagonal_movement=DiagonalMovement.never)
+                    path, pnt = finder.find_path(grid.node(start.x, start.y), grid.node(end.x, end.y), grid)
+                    path_data.append(path)
+
+            row_paths.append(path_data)
+
+        paths.append(row_paths)
+
+    return paths
+
+def ParseMap(map:List[Map]) -> (List[List[Map]], List[List[List[tuple[int,int]]]]):
     parsedMap = [[Map() for i in range(Nmap)] for j in range(Nmap)]
     for grid in map:
         parsedMap[grid.x][grid.y] = grid
-    return parsedMap
+    
+    paths = find_all_paths(parsedMap)
+    return parsedMap, paths
 
 
 # only used in play.py
