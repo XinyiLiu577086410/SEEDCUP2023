e9a73a49cb46cafcaf6e95160dc919c76e7146eb
liuxinyi <577086410@qq.com>
2023-11-18 00:08:05 +0800

    Fix: pathfinding

--- client/python/play.py
+++ client/python/play.py
@@ -13,6 +13,8 @@ import sys
 from pathfinding.core.diagonal_movement import DiagonalMovement
 from pathfinding.core.grid import Grid
 from pathfinding.finder.dijkstra import DijkstraFinder
+from pathfinding.finder.a_star import AStarFinder
+
 
 class Client(object):
     """Client obj that send/recv packet.
@@ -152,7 +154,8 @@ def Play(parsedMap: List[List[Map]], routes: List[List[List[tuple]]], playerPosi
 
 
 
-def ParseMap(map:List[Map]) -> (List[List[Map]], List[List[List[tuple]]], List[List[List[tuple]]], tuple):
+# def ParseMap(map:List[Map]) -> (List[List[Map]], List[List[List[tuple]]], List[List[List[tuple]]], tuple):
+def ParseMap(map:List[Map]) -> (List[List[Map]], List[List[List[tuple]]], tuple):
     parsedMap = [[Map() for i in range(MapEdgeLength)] for j in range(MapEdgeLength)]
     paths = [[[] for i in range(MapEdgeLength)] for j in range(MapEdgeLength)]
     accessableNow = [[1 for i in range(MapEdgeLength)] for j in range(MapEdgeLength)]
@@ -161,21 +164,37 @@ def ParseMap(map:List[Map]) -> (List[List[Map]], List[List[List[tuple]]], List[L
     for grid in map:
         parsedMap[grid.x][grid.y] = grid
         for obj in grid.objs:
-            if obj.type == ObjType.Player and obj.property.player_id == gContext["playerID"]:
+            if obj.type == ObjType.Player and obj.property.player_id != gContext["playerID"] and playerPosition is None:
                 playerPosition = (grid.x, grid.y)
             if obj.type == ObjType.Block or obj.type == ObjType.Bomb:
-                accessableNow[grid.x][grid.y] = 0
-    # an abbreviation of pathfinding grid
+                accessableNow[grid.x][grid.y] = 0 
+    # print("accessableNow:")
+    # for i in range(MapEdgeLength):
+    #     for j in range(MapEdgeLength):
+    #         print(accessableNow[i][j], end="  ")
+    #     print("\n")
+    
+    # pfGrid: an abbreviation of pathfinding grid
     pfGrid = Grid(matrix=accessableNow)
     for grid in map:
         end_position = (grid.x, grid.y)
         pfGrid.cleanup()
-        finder = DijkstraFinder(diagonal_movement=DiagonalMovement.never)
-        newPath, _ = finder.find_path(pfGrid.node(playerPosition[0], playerPosition[1]),
-                                       pfGrid.node(end_position[0], end_position[1]), pfGrid)
-        finder.
-        newPath1 = [(newPath[i].x, newPath[i].y) for i in range(len(newPath))]
-        paths[grid.x][grid.y] = newPath1
+        finder = AStarFinder(diagonal_movement=DiagonalMovement.never)
+        newPath, _ = finder.find_path(pfGrid.node(playerPosition[1], playerPosition[0]),
+                                        pfGrid.node(end_position[1], end_position[0]), pfGrid)
+        '''
+        def node(self, x, y) -> GridNode:
+        """            ^~~^~~~~~
+        get node at position
+        :param x: x pos
+        :param y: y pos
+        :return:
+        """
+        return self.nodes[y][x]
+                          ^~~^~~~~~
+        '''
+        myNewPath = [(newPath[i].y, newPath[i].x) for i in range(len(newPath))]
+        paths[grid.x][grid.y] = myNewPath
     return parsedMap, paths, playerPosition
 
 
@@ -207,8 +226,24 @@ if __name__ == "__main__":
         exit(-1)
     while(not gContext["gameOverFlag"]):
         myMap, paths, playerPosition = ParseMap(resp.data.map)
-        print(paths)
-        exit(0)
+        
+        print("map:")
+        for i in range(MapEdgeLength):
+            for j in range(MapEdgeLength):
+                if(len(myMap[i][j].objs) == 0):
+                    print("0", end="  ")
+                else:
+                    print("%d"%myMap[i][j].objs[0].type, end="  ")
+            print("\n")
+        print("paths:")
+        for i in range(MapEdgeLength):
+            for j in range(MapEdgeLength):
+                if len(paths[i][j]) == 0:
+                    print("0", end="  ")
+                else:
+                    print("1", end="  ")
+            print("\n")
+       
         requests = Play(myMap, paths, playerPosition)
         client.send(PacketReq(PacketType.ActionReq, requests))
         resp = client.recv()    
